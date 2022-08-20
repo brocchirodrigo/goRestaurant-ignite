83f9a2faea923a8c57fa9402341f5c9cf9c78407
Rodrigo Brocchi <rodrigo.brocchi@gmail.com>
2022-08-20 11:34:53 -0300

    Refactor componente food

--- src/components/Food/styles.ts
+++ src/components/Food/styles.ts
@@ -1,6 +1,10 @@
 import styled, { css } from 'styled-components';
 
-export const Container = styled.div`
+interface IProps {
+  available: Boolean;
+}
+
+export const Container = styled.div<IProps>`
   background: #f0f0f5;
   border-radius: 8px;
 
