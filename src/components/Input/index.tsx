b53154686cea322692a136fc9e5dc9f48168c2db
Rodrigo Brocchi <rodrigo.brocchi@gmail.com>
2022-08-20 12:09:10 -0300

    Refactor Input

--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -9,8 +9,14 @@ import { useField } from '@unform/core';
 
 import { Container } from './styles';
 
-const Input = ({ name, icon: Icon, ...rest }) => {
-  const inputRef = useRef(null);
+interface IProps {
+  name: string;
+  icon: any;
+}
+
+const Input = ({ name, icon: Icon, ...rest }: IProps) => {
+
+  const inputRef = useRef<HTMLInputElement>(null);
 
   const [isFocused, setIsFocused] = useState(false);
   const [isFilled, setIsFilled] = useState(false);
--- src/components/Input/styles.ts
+++ src/components/Input/styles.ts
@@ -1,6 +1,11 @@
 import styled, { css } from 'styled-components';
 
-export const Container = styled.div`
+interface IProps {
+  isFilled: boolean;
+  isFocused: boolean;
+}
+
+export const Container = styled.div<IProps>`
   display: flex;
   align-items: center;
 
